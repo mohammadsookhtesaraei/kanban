0798a54a6c06615978be3a34a8c2896fd8d7c19b
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-08-10 14:20:26 +0330

    boardPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import type { ReactNode } from 'react';
 
 import { Route, Routes } from 'react-router';
 
+import BoardPage from '@/pages/BoardPage/BoardPage';
 import HomePage from '@/pages/HomePage/HomePage';
 
 const App = (): ReactNode => {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="/board" element={<HomePage />} />
+      <Route path="/board" element={<BoardPage />} />
     </Routes>
   );
 };
---
+++ src/pages/BoardPage/BoardPage.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from 'react';
+
+const BoardPage = (): ReactNode => {
+  return <div>BoardPage</div>;
+};
+export default BoardPage;
