65803921eab0a353c5633230727ae0b10cbcbacc
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-08-19 18:02:56 +0330

    add lists reducers

--- src/providers/BoardProvider.tsx
+++ src/providers/BoardProvider.tsx
@@ -1,131 +1,51 @@
-import { type ReactNode } from 'react';
+import { type ReactNode, useEffect, useReducer } from 'react';
 
 import { listData } from '@/constant/mockData';
 
 import { BoardContext } from '@/context/board-context';
 
-import { useLocalStorage } from '@/hooks/useLocalStorage';
+import { listsReducer } from '@/reducers/lists-reducer';
 
 import type { ListType } from '@/types/list';
+
+function save(lists: ListType[]): void {
+  localStorage.setItem('lists', JSON.stringify(lists));
+}
+
+function load(): ListType[] {
+  const item = localStorage.getItem('lists');
+  if (!item) {
+    return listData;
+  }
+
+  return JSON.parse(item);
+}
 
 type Props = {
   children: ReactNode;
 };
 
 const BoardProvider = ({ children }: Props): ReactNode => {
-  const [lists, setLists] = useLocalStorage<ListType[]>('lists', listData);
+  const [lists, dispatch] = useReducer(listsReducer, load());
+
+  useEffect(() => {
+    save(lists);
+  }, [lists]);
 
   const create = (): void => {
-    setLists((prev) => {
-      const clone = [...prev];
-
-      const id = globalThis.crypto.randomUUID();
-
-      clone[0] = { ...clone[0], items: [...clone[0].items, { id, title: id }] };
-      return clone;
-    });
+    dispatch({ type: 'created' });
   };
 
   const move = (
-    destinationListId: string,
+    destinationId: string,
     listId: string,
     itemId: string
   ): void => {
-    setLists((prev) => {
-      // find index active list in array lists state
-      const activeListIndex = prev.findIndex((list) => list.id === listId);
-
-      // find index listdestination in array lists state because we want move item
-      const destinationListIndex = prev.findIndex(
-        (list) => list.id === destinationListId
-      );
-
-      // validation
-      if (activeListIndex === -1 || destinationListIndex === -1) {
-        console.error('cannot find desire list');
-        return prev;
-      }
-
-      // clone
-      const clone = [...prev];
-
-      // active list
-      const activeList = {
-        ...clone[activeListIndex],
-        items: [...clone[activeListIndex].items],
-      };
-
-      // find item in active list
-      const activeItemIdIndex = activeList.items.findIndex(
-        (list) => list.id === itemId
-      );
-
-      // destination list
-      const destinationList = {
-        ...clone[destinationListIndex],
-        items: [...clone[destinationListIndex].items],
-      };
-
-      // validation
-      if (activeItemIdIndex === -1) {
-        console.error('cannot find desire list');
-        return prev;
-      }
-
-      // get remove item from actice list items
-      const [activeItem] = activeList.items.splice(activeItemIdIndex, 1);
-
-      // add remove item to destination list items
-      destinationList.items.push(activeItem);
-      // new active list
-      clone[activeListIndex] = activeList;
-      // new destination list
-      clone[destinationListIndex] = destinationList;
-      // return clone
-      return clone;
-    });
+    dispatch({ type: 'moved', destinationId, listId, itemId });
   };
 
   const remove = (listId: string, itemId: string): void => {
-    setLists((prev) => {
-      // find active list index
-      const activeListIndex = prev.findIndex(
-        (list) => list.id === listId /* state */
-      );
-
-      // validation
-      if (activeListIndex === -1) {
-        console.error('cannot find desire list');
-        return prev;
-      }
-
-      // clone array list
-      const clone = [...prev];
-
-      // clone - acitve list object from array
-      const activeList = {
-        ...clone[activeListIndex],
-        items: [...clone[activeListIndex].items],
-      };
-
-      // find active item in activeList {id:"",title:"" ,items:[]}
-      const activeItemIdIndex = activeList.items.findIndex(
-        (list) => list.id === itemId /* state */
-      );
-
-      // validation
-      if (activeItemIdIndex === -1) {
-        console.error('cannot find desire list');
-        return prev;
-      }
-
-      // remove item from activelist - items
-      activeList.items.splice(activeItemIdIndex, 1);
-
-      // new activeList without remove item
-      clone[activeListIndex] = activeList;
-      return clone;
-    });
+    dispatch({ type: 'removed', listId, itemId });
   };
 
   return (
---
+++ src/reducers/lists-reducer.ts
@@ -0,0 +1,127 @@
+import type { ListType } from '@/types/list';
+
+type Action =
+  | {
+      type: 'created';
+    }
+  | {
+      type: 'moved';
+      destinationId: string;
+      itemId: string;
+      listId: string;
+    }
+  | {
+      type: 'removed';
+      itemId: string;
+      listId: string;
+    };
+
+export function listsReducer(state: ListType[], action: Action): ListType[] {
+  switch (action.type) {
+    case 'created': {
+      const clone = [...state];
+      const id = globalThis.crypto.randomUUID();
+      clone[0] = { ...clone[0], items: [...clone[0].items, { id, title: id }] };
+      return clone;
+    }
+
+    case 'moved': {
+      // find index active list in array lists state
+      const activeListIndex = state.findIndex(
+        (list) => list.id === action.listId
+      );
+
+      // find index listdestination in array lists state because we want move item
+      const destinationListIndex = state.findIndex(
+        (list) => list.id === action.destinationId
+      );
+
+      // validation
+      if (activeListIndex === -1 || destinationListIndex === -1) {
+        console.error('cannot find desire list');
+        return state;
+      }
+
+      // clone
+      const clone = [...state];
+
+      // active list
+      const activeList = {
+        ...clone[activeListIndex],
+        items: [...clone[activeListIndex].items],
+      };
+
+      // find item in active list
+      const activeItemIdIndex = activeList.items.findIndex(
+        (list) => list.id === action.itemId
+      );
+
+      // destination list
+      const destinationList = {
+        ...clone[destinationListIndex],
+        items: [...clone[destinationListIndex].items],
+      };
+
+      // validation
+      if (activeItemIdIndex === -1) {
+        console.error('cannot find desire list');
+        return state;
+      }
+
+      // get remove item from actice list items
+      const [activeItem] = activeList.items.splice(activeItemIdIndex, 1);
+
+      // add remove item to destination list items
+      destinationList.items.push(activeItem);
+      // new active list
+      clone[activeListIndex] = activeList;
+      // new destination list
+      clone[destinationListIndex] = destinationList;
+      // return clone
+      return clone;
+    }
+    case 'removed': {
+      // find active list index
+      const activeListIndex = state.findIndex(
+        (list) => list.id === action.listId /* state */
+      );
+
+      // validation
+      if (activeListIndex === -1) {
+        console.error('cannot find desire list');
+        return state;
+      }
+
+      // clone array list
+      const clone = [...state];
+
+      // clone - acitve list object from array
+      const activeList = {
+        ...clone[activeListIndex],
+        items: [...clone[activeListIndex].items],
+      };
+
+      // find active item in activeList {id:"",title:"" ,items:[]}
+      const activeItemIdIndex = activeList.items.findIndex(
+        (list) => list.id === action.itemId /* state */
+      );
+
+      // validation
+      if (activeItemIdIndex === -1) {
+        console.error('cannot find desire list');
+        return state;
+      }
+
+      // remove item from activelist - items
+      activeList.items.splice(activeItemIdIndex, 1);
+
+      // new activeList without remove item
+      clone[activeListIndex] = activeList;
+      return clone;
+    }
+
+    default: {
+      throw new Error('unknown action!');
+    }
+  }
+}
