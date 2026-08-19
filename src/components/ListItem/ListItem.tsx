cd1c2fa0cf8b7fc108478d807519199c10bfc833
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-08-19 13:42:34 +0330

    add remove icon

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,5 +1,9 @@
 import type { ReactNode } from 'react';
 
+import IconButton from '@/components/IconButton/IconButton';
+
+import MingcuteDelete2Line from '@/icons/MingcuteDelete2Line';
+
 import type { ListItemType } from '@/types/list-item';
 
 import styles from './ListItem.module.css';
@@ -8,12 +12,16 @@ type Props = {
   listId: string;
   item: ListItemType;
   onClick?: (listId: string, itemId: string) => void;
+  onRemove?: (listId: string, itemId: string) => void;
 };
 
 const ListItem = ({ item, onClick, listId }: Props): ReactNode => {
   return (
     <div className={styles.item} onClick={() => onClick?.(listId, item.id)}>
       {item.title}
+      <IconButton>
+        <MingcuteDelete2Line />
+      </IconButton>
     </div>
   );
 };
