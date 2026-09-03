0b12452ae431267ca795da0343a9d83efb3b9d34
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-09-03 12:48:07 +0330

    add react compiler for fps

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import react from '@vitejs/plugin-react';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+import babel from '@rolldown/plugin-babel';
 import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
+  ],
   server: {
     port: 3000,
     open: true,
